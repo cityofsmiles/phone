f6637a14f3ff3854dc43bb784914517011e0af58
cityofsmiles <cityofsmiles@gmail.com>
2021-01-08 20:10:34 +0800

    'added git setup'

--- bash-scripts/setup-git.sh
+++ bash-scripts/setup-git.sh
@@ -5,3 +5,10 @@ git config --global user.email "cityofsmiles@gmail.com"
 git config --global user.name "cityofsmiles"
 
 git config --global pull.rebase false
+
+cd /home/jonathan/Documents/phone
+
+git remote -v
+
+git remote set-url origin git@github.com:cityofsmiles/phone.git
+
