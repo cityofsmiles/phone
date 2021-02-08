1c471b0bc9317fdfeb062b38123b473179481d6e
cityofsmiles <cityofsmiles@gmail.com>
2021-02-08 10:21:39 +0800

    'ok sxmo'

--- bash-scripts/pine-install-apps.sh
+++ bash-scripts/pine-install-apps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sudo apk add xfce4-terminal telegram-desktop okular vlc rsync inotify-tools texmaker
+sudo apk add xfce4-terminal telegram-desktop okular vlc rsync inotify-tools texmaker pidgin purple-xmpp-http-upload purple-facebook
+
+
 
 
 
--- bash-scripts/setup-sdcard.sh
+++ bash-scripts/setup-sdcard.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-sudo chown /dev/mmcblk0
+#sudo chown -R jonathan /dev/mmcblk0
 
-#sudo mkdir /mnt/sdcard
+sudo chown -R jonathan /mnt/sdcard
 
-#sudo mount /dev/mmcblk0p1 /mnt/sdcard
+sudo mkdir /mnt/sdcard
 
-sudo mkdir -p /media/mmcblk0p1/Documents 
+sudo mount /dev/mmcblk0p1 /mnt/sdcard
 
-sudo mkdir -p /media/mmcblk0p1/Downloads
+sudo mkdir -p /mnt/sdcard/Documents 
+
+sudo mkdir -p /mnt/sdcard/Downloads
+
+#sudo mkdir -p /media/mmcblk0p1/Documents 
+
+#sudo mkdir -p /media/mmcblk0p1/Downloads
+
 
 rm -r ~/Documents
 
 rm -r ~/Downloads
 
-ln -s /media/mmcblk0p1/Documents ~/Documents
+#ln -s /media/mmcblk0p1/Documents ~/Documents
 
-ln -s /media/mmcblk0p1/Downloads ~/Downloads
+#ln -s /media/mmcblk0p1/Downloads ~/Downloads
+
+ln -s /mnt/sdcard/Documents ~/Documents
+
+ln -s /mnt/sdcard/Downloads ~/Downloads
 
 
