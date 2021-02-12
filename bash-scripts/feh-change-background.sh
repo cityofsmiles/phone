3603c8545d12863d3cbd250cd54e1c24c57cbfc6
cityofsmiles <cityofsmiles@gmail.com>
2021-02-12 12:55:34 +0800

    'ok wallpaper'

--- bash-scripts/feh-change-background.sh
+++ bash-scripts/feh-change-background.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/bash
-img=(`find /home/jonathan/Documents/wallpapers -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+#!/bin/sh
+
+#img=(`find /home/jonathan/Documents/wallpapers -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+
 while true
 do
-   feh --bg-scale "${img[$RANDOM % ${#img[@]} ]}"
-sleep 10m
+
+feh --bg-fill --randomize ~/Documents/phone/wallpapers/*
+
+#   feh --bg-scale "${img[$RANDOM % ${#img[@]} ]}"
+#sleep 10m
 done
