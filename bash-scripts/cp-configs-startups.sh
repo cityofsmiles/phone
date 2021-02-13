6047ccd4085adfd63b4127c84e3f987ff22c1284
cityofsmiles <cityofsmiles@gmail.com>
2021-02-13 22:12:45 +0800

    'all ok'

--- bash-scripts/cp-configs-startups.sh
+++ bash-scripts/cp-configs-startups.sh
@@ -9,3 +9,7 @@ cp ~/Documents/phone/configs/rc.conf ~/.config/ranger/rc.conf
 #sudo cp -r ~/Documents/phone/pmos-init-files/* /etc/init.d/
 
 #cp ~/Documents/phone/configs/bashrc ~/.bashrc
+
+cp ~/.purple/blist.xml ~/Documents/phone/configs/blist.xml
+
+cp ~/.purple/accounts.xml ~/Documents/phone/configs/accounts.xml
