c713d7fe9ad4f591f0cbc96877b3bd8dffa78bef
cityofsmiles <cityofsmiles@gmail.com>
2021-02-15 09:45:27 +0800

    'ok'

--- bash-scripts/cp-configs-startups.sh
+++ bash-scripts/cp-configs-startups.sh
@@ -6,7 +6,7 @@ cp ~/Documents/phone/configs/pinephone-profile ~/.profile;
 
 cp ~/Documents/phone/configs/rc.conf ~/.config/ranger/rc.conf
 
-#sudo cp -r ~/Documents/phone/pmos-init-files/* /etc/init.d/
+sudo cp -r ~/Documents/phone/pmos-init-files/* /etc/init.d/
 
 #cp ~/Documents/phone/configs/bashrc ~/.bashrc
 
