dcfed29409680bd84cb38122a8a6064ec24f9f25
cityofsmiles <cityofsmiles@gmail.com>
2021-03-21 16:42:56 +0800

    'ok'

--- bash-scripts/pacman-install-apps.sh
+++ bash-scripts/pacman-install-apps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S xfce4-terminal okular vlc rsync inotify-tools texstudio mousepad github-cli pidgin pidgin-xmpp-receipts pidgin-hotkeys pidgin-libnotify purple-facebook purple-plugin-pack libreoffice chromium purple-telegram telegram-desktop #yay flatpak texlive-most
+sudo pacman -S xfce4-terminal okular vlc rsync inotify-tools texstudio mousepad github-cli pidgin pidgin-xmpp-receipts pidgin-hotkeys pidgin-libnotify purple-facebook purple-plugin-pack libreoffice chromium purple-telegram telegram-desktop quickddit #yay flatpak texlive-most
 
 #sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
